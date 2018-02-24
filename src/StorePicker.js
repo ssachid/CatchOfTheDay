fdc6c18389794c3da3cea0855c5f1e235849e40d
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-24 15:04:10 -0800

    Implemented goToStore function with route transition

--- src/StorePicker.js
+++ src/StorePicker.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { getFunName } from './helpers'
 
 class StorePicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.goToStore = this.goToStore.bind(this)
+  }
+
+  goToStore(e) {
+    e.preventDefault();
+    const storeId = this.storeInput
+    this.props.history.push(`/store/${storeId}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2> Please Enter a Store </h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input type="text"
+               required
+               placeholder="Store Name"
+               defaultValue={getFunName()}
+               ref = { (input) => (this.storeInput = input) } />
         <button type="submit"> Visit Store </button>
       </form>
     )
