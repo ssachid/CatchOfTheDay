5371c9cf093dfb8bd9f00e03a2ca587df5034070
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-24 15:43:05 -0800

    Added Load samples button to Inventory

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
 
+
 class Inventory extends React.Component {
+
   render() {
     return (
       <div>
         <p>Inventory</p>
         <AddFishForm addFish = {this.props.addFish} />
+        <button onClick={this.props.loadSamples}>Load Fishes </button>
       </div>
     )
   }
