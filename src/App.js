aba979be20c9c23fd9afd66f5f65bdb61f565068
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-24 13:38:44 -0800

    Initiated Header, Inventory, Order and StorePicker components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
+import './css/style.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import StorePicker from './StorePicker'
 
 const Root = () => {
   return (
-    "hello"
+    <StorePicker />
   )
 }
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+render(<App />, document.getElementById('main'));
 registerServiceWorker();
