d7a29f0f512f41cd6fef9c6d0fc2e8abba9f9ff6
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-25 22:40:04 -0800

    Defined renderOrder function to render each order

--- src/Order.js
+++ src/Order.js
@@ -2,6 +2,27 @@ import React from 'react';
 import { formatPrice } from './helpers'
 
 class Order extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderOrder = this.renderOrder.bind(this);
+  }
+
+  renderOrder(key) {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+
+    if ( !fish || fish.status === 'unavailable') {
+      return <li key={key}> Sorry, { fish ? fish.name : 'fish'} is no longer available! </li>
+    }
+
+    return (
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{formatPrice(count* fish.price)}</span>
+      </li>
+    )
+  }
+
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce((prevCount, key) => {
