ac195ba5b0d1a4b4e967c85bb46f84b5eac2de15
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-27 19:51:51 -0800

    Passed updateFish function as props to the Inventory component

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends React.Component {
              }
         </div>
         <Order fishes = {this.state.fishes} order={this.state.order} history = {this.props.history.location} />
-        <Inventory addFish = {this.addFish} loadSamples = { this.loadSamples } fishes={this.state.fishes} />
+        <Inventory addFish = {this.addFish} updateFish = {this.updateFish} loadSamples = { this.loadSamples } fishes={this.state.fishes} />
       </div>
     )
   }
